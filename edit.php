d6f6d32a47e877349b973a3b9887e235b49d3391
DeonBeka <deonbeka92@gmail.com>
2025-06-16 19:18:41 +0200

    ./.

---
+++ edit.php
@@ -0,0 +1,54 @@
+<?php
+
+include_once('config.php');
+
+$id = $_GET['id'];
+$sql = "SELECT * FROM users Where id=:id";
+
+$prep = $conn->prepare($sql);
+$prep->bindParam(':id',$id);
+
+$prep->execute();
+
+$data = $prep->fetch();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+        form>input{
+            margin-bottom:10px;
+            font-size:20px;
+            padding:5px;
+        }
+        button{
+            background:none;
+            border:none;
+            border:1px solid black;
+            font-size:20px;
+            cursor: pointer;
+        }
+    </style>
+
+    <form action="update.php" method="POST">
+        <input type="hidden" name="id" value="<?php echo $data['id']?>"><br>
+        <input type="text" name="name" value="<?php echo $data['name']?>"><br>
+        <input type="text" name="surname" value="<?php echo $data['surname']?>"><br>
+        <input type="email" name="email" value="<?php echo $data['email']?>"><br>
+
+        <br><br>
+
+        <button type="submit" name="update">UPDATE</button>
+
+    </form>
+
+    <a href="dashboard.php"></a>
+</body>
+</html>
